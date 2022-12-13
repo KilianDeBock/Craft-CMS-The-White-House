446e708ec90c2d1de989b4ef605954e295506377
Kilian De Bock <kilian.debock@student.arteveldehs.be>
2022-12-13 09:41:46 +0100

    22.50.2.2: Initialize app

---
+++ webpack.mix.js
@@ -0,0 +1,10 @@
+const mix = require("laravel-mix");
+
+mix
+  .js("./src/scripts/index.js", "app.js")
+  .sass("./src/styles/main.scss", "app.css")
+  .setPublicPath("./web/dist")
+  .version();
+
+// mix.browserSync({ proxy: "https://craft-white-house.ddev.site", https: true });
+mix.browserSync("https://craft-white-house.ddev.site");
