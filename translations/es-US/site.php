c6e40192093992b14057e33e11a77781f034907c
Kilian De Bock <kilian.debock@student.arteveldehs.be>
2022-12-29 16:41:49 +0100

    22.52.4.4: Main navigation: add desktop support

---
+++ translations/es-US/site.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Menu' => 'Menú',
+    'Close' => 'Cerrar',
+    'EN' => 'English',
+    'ES' => 'Español',
+    'addressName' => 'The White House',
+    'addressStreet' => '1600 Pennsylvania Ave NW',
+    'addressCity' => 'Washington, DC 20500',
+];
