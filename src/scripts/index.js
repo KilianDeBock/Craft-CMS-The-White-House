fcc34f2b09d1b823d6176bcf469e588f478eea93
Kilian De Bock <kilian.debock@student.arteveldehs.be>
2022-12-29 13:19:12 +0100

    22.52.4.3: Styled Mobile navigation (basic)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,27 @@
-console.log("Alert");
+(() => {
+  const app = {
+    initialize() {
+      console.log("App initialized!");
+      this.cacheElements();
+      this.registerListeners();
+    },
+    cacheElements() {
+      this.$body = document.querySelector("body");
+      this.$mainNavInput = document.querySelector("#main-nav__opener");
+      console.log("Eventlisteners added!");
+    },
+    registerListeners() {
+      const scrollbarWidth = this.$body.offsetWidth - this.$body.clientWidth;
+      console.log(scrollbarWidth);
+      this.$mainNavInput.addEventListener("click", (event) => {
+        if (event.target.checked) {
+          return (this.$body.style = "overflow: hidden");
+        }
+        return (this.$body.style = "");
+      });
+      console.log("Eventlisteners added!");
+    },
+  };
+
+  app.initialize();
+})();
