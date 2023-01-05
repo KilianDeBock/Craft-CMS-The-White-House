e13785527d7160d053a6bcb58b84bcefb5153f7c
Kilian De Bock <kilian.debock@student.arteveldehs.be>
2023-01-05 10:03:42 +0100

    23.1.4.1: Update logos & logo names

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,10 @@
+document.write(
+  "<script async src='/browser-sync/browser-sync-client.2.10.1.js'></script>".replace(
+    "HOST",
+    location.hostname
+  )
+);
+
 (() => {
   const app = {
     initialize() {
